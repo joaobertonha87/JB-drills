236c32d9bb9bafc898187aaa808759aeaf6944f3
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-09-05 18:58:20 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Editor from "./pages/Editor";
+import "./style.css";
+
+createRoot(document.getElementById("root")).render(<Editor />);
