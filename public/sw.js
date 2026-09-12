cd3a676e7bc2c0f94f1b6a618a7a14db472a23c5
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-09-11 22:20:03 -0300

    Delete public directory

--- public/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='jb-tactics-v1-54-9-true-single-arena';
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}));});
