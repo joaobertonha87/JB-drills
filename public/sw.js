96531cd69838c4cb0df6c526cd286ae12d4b94eb
joaobertonha87 <joaoleiteribeiro87@gmail.com>
2026-09-11 21:38:42 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,11 @@
-const CACHE='jb-tactics-v1-54-7-single-source';
+const CACHE='jb-tactics-v1-54-8-premium-arena-single-layer';
 self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request,{cache:'no-store'}));});
+self.addEventListener('activate',e=>e.waitUntil(
+ caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+ .then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>{
+ if(e.request.method==='GET'){
+   e.respondWith(fetch(e.request,{cache:'no-store'}));
+ }
+});
